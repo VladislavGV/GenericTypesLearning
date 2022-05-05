e2f8d27a2a72ec29bbd578d7b51d3d968bbc8dd8
VladislavGV <0377722@mail.ru>
2022-05-05 13:58:40 +0300

    GenericTypes with deep 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import javax.management.ObjectName;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
@@ -27,8 +28,8 @@ public class Main {
 
         Object[] objects = {10, "Привет", 3.14}; // здесь происходит автоупаковка
 
-        for(Object o : objects) {
-            if(o instanceof String){
+        for (Object o : objects) {
+            if (o instanceof String) {
                 String s = (String) o;
                 System.out.println(s);
 
@@ -38,21 +39,51 @@ public class Main {
 
         ArrayList<Integer> numbers = new ArrayList(); // Сохраняет Object\
 
-        for(int i = 0; i < 10; i++){ //сохранять коллекцию числа 10, 20 .. 100
-            numbers.add(i*10);
+        for (int i = 0; i < 10; i++) { //сохранять коллекцию числа 10, 20 .. 100
+            numbers.add(i * 10);
 //            numbers.add(i*10.f);
         }
 //        numbers.add(10.4F);
 
         int sum = 0;
-        for(Integer o : numbers){
+        for (Integer o : numbers) {
             sum = sum + /*(Integer)*/ o;
         }
         System.out.println(sum);
 
 
-        // Основной тип<Тип параметра>
+        // Основной тип<ТипПараметр>
+        // Основной тип<ТипПараметр1, ТипПараметр2, ТипПараметр3....>
 
+        HashMap<Integer, String> map = new HashMap<Integer, String>();
+        map.put(7, "Привет");
+        map.put(15, "Hello");
 
+        ArrayList<String> listHello = new ArrayList<>();
+        listHello.add("Привет");
+        listHello.add("Hi");
+
+        ArrayList<String> listBye = new ArrayList<>();
+        listBye.add("Пока");
+        listBye.add("Good Bye");
+
+        ArrayList<ArrayList<String>> list = new ArrayList<>();
+        list.add(listHello);
+        list.add(listBye);
+
+        for (ArrayList<String> spisok : list) {
+            for (String s : spisok) {
+                System.out.println(s);
+            }
+        }
     }
 }
+
+/*
+*        то что делаем в коде                               то во что преобразует компилятор
+* ArrayList<Integer> list = new ArrayList<Integer>();       ArrayList = new ArrayList
+*  list.add(1);                                             list.add((Integer) 1);
+* int x = list.get(0);                                      int x = (Integer) list.get(0);
+* list.set(0,10);                                           list.set(0, (Integer) 10);
+*
+* */

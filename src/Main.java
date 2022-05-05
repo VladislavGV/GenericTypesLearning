fa4bca7ef96cccb211292591893fa713d259ccad
VladislavGV <0377722@mail.ru>
2022-05-05 11:57:28 +0300

    Add Instanceof

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import javax.management.ObjectName;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Object o = new Scanner(System.in);
 
+
+
+        System.out.println(o instanceof Scanner);
+        ((String) o ).length();
+
+        Object o1 = new String();
+
+        Object o2 = new Integer(15);
 
     }
 }
